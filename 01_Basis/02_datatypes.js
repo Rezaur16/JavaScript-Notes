27d451fbd862e8a8d10d6aecbb9ecb775ee8f5a8
Rezaur Rahman Ratul <88153639+Rezaur16@users.noreply.github.com>
2024-07-11 13:03:55 +0000

    datatypes

--- 01_Basis/02_datatypes.js
+++ 01_Basis/02_datatypes.js
@@ -1,6 +1,34 @@
 //"user strict"//treat all Js code as newer version
-alert("hello")// we are using nodejs not the the browser
+//alert("hello")// we are using nodejs not the the browser
  console.log("3+3");  console.log("Hello"); // code Readablity should be high
  //Prettier - Code formatter it can format the code 
  // For documentation :https://tc39.es/ecma262/#sec-intro
-  
+//Primitive Datatypes
+//------------------------------------------------------------------
+ let name="hitesh"
+ let age=18
+ //let isLoggedIn=False // boolean
+ let state;
+ // undefined 
+ let state1=null
+ // suppose in the fridge if there is no tempature than its return 0 but 0 is already temparature 
+ // so in that case we use null
+ // number => 2 to power 53
+ // bigint==>                // share market
+ // string => ""
+// boolearn => true /false
+// null => standalone value 
+// undefined => 
+// symbol =>unique
+//-----------------------------------------------
+
+
+console.log(typeof "hitesh");
+// string
+console.log(typeof null);
+//object
+// Null is one type of object 
+console.log(typeof undefined);
+//undefined 
+console.log(typeof age);
+// number
