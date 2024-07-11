ea3253083d78b67ca81ce50f76d7b1cad9cae966
Rezaur Rahman Ratul <88153639+Rezaur16@users.noreply.github.com>
2024-07-11 12:48:10 +0000

    learn

---
+++ 01_Basis/02_datatypes.js
@@ -0,0 +1,6 @@
+//"user strict"//treat all Js code as newer version
+alert("hello")// we are using nodejs not the the browser
+ console.log("3+3");  console.log("Hello"); // code Readablity should be high
+ //Prettier - Code formatter it can format the code 
+ // For documentation :https://tc39.es/ecma262/#sec-intro
+  
